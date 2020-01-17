99ce097c41f92a23cdc20220ece7e6ac7b86f5d3
unknown <lukas.paes18@gmail.com>
2020-01-17 11:22:13 -0300

    SignIn refeita

--- src/Pages/Components/index.js
+++ src/Pages/Components/index.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-around;
   flex-direction: column;
   height: 100vh;
 `;
@@ -16,39 +16,10 @@ export const Form = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 500px;
-  background: rgb(2, 99, 44);
-  border-radius: 5px;
-  margin-top: 5%;
-  margin-bottom: 5%;
 
-  div {
-    width: 90%;
-    margin-top: 5%;
+  width: 400px;
+  border-radius: 10px;
+  padding: 30px;
 
-    input {
-      background-color: #fff;
-      border: none;
-      font-size: 18px;
-      height: 45px;
-      width: 100%;
-      margin-top: 2%;
-      border-radius: 5px;
-      &::placeholder {
-        color: #999;
-      }
-    }
-  }
-
-  button {
-    font-size: 20px;
-    height: 45px;
-    width: 90%;
-    border: none;
-    margin-top: 10%;
-    margin-bottom: 10%;
-    border-radius: 5px;
-    background-color: rgb(255, 255, 255);
-    color: #848484;
-  }
+  background: rgba(254, 254, 254, 0.9);
 `;
