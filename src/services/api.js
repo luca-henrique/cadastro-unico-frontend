7fe9a09e8f36162715556a522a703fd3b5d83d49
luca.henrique <lukas.paes18@gmail.com>
2020-02-21 14:17:57 -0300

    Direto na master

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { store } from "../store/index";
 
 const api = axios.create({
-  baseURL: "http://192.168.0.110:3333"
+  baseURL: process.env.REACT_APP_API_URL
 });
 
 api.interceptors.request.use(config => {
