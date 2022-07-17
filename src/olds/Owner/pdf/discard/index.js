6640b8e4e34de0920e6ea5a23b9ca3860632ac1b
Lucas Henrique <lukas.paes18@gmail.com>
2022-07-17 20:17:23 -0300

    subir codigo

--- src/olds/Owner/pdf/discard/index.js
+++ src/olds/Owner/pdf/discard/index.js
@@ -1,22 +1,18 @@
-import React, { useEffect } from "react";
+import React, {useEffect} from 'react';
 
+import {Page, Text, View, Document, Image} from '@react-pdf/renderer';
+import {PDFViewer} from '@react-pdf/renderer';
 
-import { Page, Text, View, Document, Image } from "@react-pdf/renderer";
-import { PDFViewer } from "@react-pdf/renderer";
+import Loader from '~/components/loader/';
 
-
-import Loader from "~/components/loader/";
-
-import Logo from "../../../../Assets/Images/ctm-logo.png";
-
-import moment from "moment";
+import moment from 'moment';
 
 const PDF = (props) => {
-  const { discard_boxes, discard_boxes_loading } = props.redux.generete;
+  const {discard_boxes, discard_boxes_loading} = props.redux.generete;
 
-  const { prefecture } = props.redux.prefecture;
+  const {prefecture} = props.redux.prefecture;
 
-  const { generatePdfDiscardRequest } = props;
+  const {generatePdfDiscardRequest} = props;
 
   useEffect(() => {
     if (discard_boxes_loading) {
@@ -26,14 +22,14 @@ const PDF = (props) => {
 
   function dataAtualFormatada() {
     var data = new Date(),
-      dia = data.getDate().toString().padStart(2, "0"),
-      mes = (data.getMonth() + 1).toString().padStart(2, "0"), //+1 pois no getMonth Janeiro começa com zero.
+      dia = data.getDate().toString().padStart(2, '0'),
+      mes = (data.getMonth() + 1).toString().padStart(2, '0'), //+1 pois no getMonth Janeiro começa com zero.
       ano = data.getFullYear();
-    return dia + "/" + mes + "/" + ano;
+    return dia + '/' + mes + '/' + ano;
   }
 
   function formatDate(data) {
-    const date = moment(data).format("DD/MM/YYYY");
+    const date = moment(data).format('DD/MM/YYYY');
     return date;
   }
 
@@ -44,105 +40,102 @@ const PDF = (props) => {
   }
 
   return (
-    <PDFViewer style={{ width: "100%", height: "100%" }}>
-      <Document title="Relatorio geral" orientation="portrait">
-        <Page size="A3">
+    <PDFViewer style={{width: '100%', height: '100%'}}>
+      <Document title='Relatorio geral' orientation='portrait'>
+        <Page size='A3'>
           <View
             style={{
-              display: "flex",
-              flexDirection: "column",
-              flexWrap: "wrap",
-              width: "100%",
-              height: "100%",
+              display: 'flex',
+              flexDirection: 'column',
+              flexWrap: 'wrap',
+              width: '100%',
+              height: '100%',
             }}
           >
             <View
               style={{
-                display: "flex",
-                flexDirection: "row",
-                margin: "5px",
+                display: 'flex',
+                flexDirection: 'row',
+                margin: '5px',
               }}
             >
               <View
                 style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  width: "70%",
+                  display: 'flex',
+                  flexDirection: 'column',
+                  width: '70%',
                 }}
               >
                 <View>
                   <Text>{prefecture.nome}</Text>
                 </View>
-                <View style={{ width: "70px", height: "70px" }}>
-                  <Image src={Logo} />
-                </View>
               </View>
               <View
                 style={{
-                  display: "flex",
-                  flexDirection: "column",
+                  display: 'flex',
+                  flexDirection: 'column',
 
-                  width: "30%",
+                  width: '30%',
                 }}
               >
                 <View>
                   <Text>{date}</Text>
                 </View>
-                <View style={{ marginTop: "30px" }}>
+                <View style={{marginTop: '30px'}}>
                   <Text>Relatorio de descarte</Text>
                 </View>
               </View>
             </View>
             <View
               style={{
-                display: "flex",
-                flexDirection: "row",
-                marginTop: "20px",
-                justifyContent: "space-around",
+                display: 'flex',
+                flexDirection: 'row',
+                marginTop: '20px',
+                justifyContent: 'space-around',
               }}
             >
-              <View style={{ width: " 10%" }}>
+              <View style={{width: ' 10%'}}>
                 <Text>pasta</Text>
               </View>
-              <View style={{ width: " 10%" }}>
+              <View style={{width: ' 10%'}}>
                 <Text>caixa</Text>
               </View>
-              <View style={{ width: " 12%" }}>
+              <View style={{width: ' 12%'}}>
                 <Text>entrevista</Text>
               </View>
-              <View style={{ width: " 16%" }}>
+              <View style={{width: ' 16%'}}>
                 <Text>cod.domiciliar</Text>
               </View>
-              <View style={{ width: " 12%" }}>
+              <View style={{width: ' 12%'}}>
                 <Text>situação</Text>
               </View>
             </View>
 
             <View
               style={{
-                display: "flex",
-                flexDirection: "row",
-                marginTop: "20px",
-                justifyContent: "space-around",
+                display: 'flex',
+                flexDirection: 'row',
+                marginTop: '20px',
+                justifyContent: 'space-around',
               }}
             >
               {discard_boxes.map((descarte) => (
                 <>
-                  <View style={{ width: " 10%" }}>
+                  <View style={{width: ' 10%'}}>
                     <Text>{descarte.numPaste}</Text>
                   </View>
-                  <View style={{ width: " 10%" }}>
+                  <View style={{width: ' 10%'}}>
                     <Text>{descarte.numBox}</Text>
                   </View>
-                  <View style={{ width: " 12%" }}>
+                  <View style={{width: ' 12%'}}>
                     <Text>{formatDate(descarte.dateInterview)}</Text>
                   </View>
-                  <View style={{ width: " 16%" }}>
+                  <View style={{width: ' 16%'}}>
                     <Text>{descarte.codHome}</Text>
                   </View>
-                  <View style={{ width: " 12%" }}>
+                  <View style={{width: ' 12%'}}>
                     <Text>
-                      {descarte.situation === true ? "ativa" : "desativada"}
+                      {descarte.situation === true ? 'ativa' : 'desativada'}
                     </Text>
                   </View>
                 </>
@@ -155,6 +148,4 @@ const PDF = (props) => {
   );
 };
 
-
-
-export default (PDF);
+export default PDF;
